947fb99763563c97266ba9d63f949cfb13ad1393
MG <91797601+My-Grandin@users.noreply.github.com>
2023-05-10 21:49:16 +0200

    Made entity properies nullable

--- CatHome.Domain/Entities/Cat.cs
+++ CatHome.Domain/Entities/Cat.cs
@@ -6,16 +6,16 @@ namespace CatHome.Domain.Entities
     {
         public int Id { get; set; }
         public string Name { get; set; } = null!;
-        public DateTime? Birthdate { get; set; } 
+        public DateTime Birthdate { get; set; } 
         public string Color { get; set; } = null!;
         public string Sex { get; set; } = null!;
         [JsonIgnore]
         [JsonPropertyName("townId")]
         public int Town { get; set; }
         public Town City { get; set; } = null!;
-        public List<string> Personality { get; set; } = null!;
+        public List<string>? Personality { get; set; }
         public string? Disability { get; set; }
-        public List<int> LivesWith { get; set; } = null!;
+        public List<int>? LivesWith { get; set; }
 
     }
 }
